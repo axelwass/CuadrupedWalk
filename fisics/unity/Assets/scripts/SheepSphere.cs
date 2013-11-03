7163d1ce461da7879233e3b5d7fe68f278dbc74b
kvnknny <knny.kvn@gmail.com>
2013-11-03 17:38:44 -0300

    Spheres scene

---
+++ fisics/unity/Assets/scripts/SheepSphere.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepSphere : MonoBehaviour {
+	
+	public GameObject sheep1;
+	public GameObject sheep2;
+
+	// Use this for initialization
+	void Start () {
+		sheep1.rigidbody.AddForce(new Vector3(0, 0, -2), ForceMode.Impulse);
+		sheep2.rigidbody.AddForce(new Vector3(0, 0, 2), ForceMode.Impulse);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
