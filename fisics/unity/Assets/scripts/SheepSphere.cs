796af471992233b52ad562c92c1d3ecf4c59293b
kvnknny <knny.kvn@gmail.com>
2013-11-05 18:15:11 -0300

    Speheres colission

--- fisics/unity/Assets/scripts/SheepSphere.cs
+++ fisics/unity/Assets/scripts/SheepSphere.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class SheepSphere : MonoBehaviour {
 	
 	public GameObject sheep1;
 	public GameObject sheep2;
+	public int force;
+	StreamWriter writer;
 
 	// Use this for initialization
 	void Start () {
-		sheep1.rigidbody.AddForce(new Vector3(0, 0, -2), ForceMode.Impulse);
-		sheep2.rigidbody.AddForce(new Vector3(0, 0, 2), ForceMode.Impulse);
-			
+		sheep1.rigidbody.AddForce(new Vector3(0, 0, -force), ForceMode.Impulse);
+		sheep2.rigidbody.AddForce(new Vector3(0, 0, force), ForceMode.Impulse);
+		
+       writer = new StreamWriter("C:/Users/Kevin/Desktop/Test.txt");
+	   writer.WriteLine("Time \t Sheep1(z) \t Sheep2(z)");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if(Time.time < 5){
+	   writer.WriteLine(Time.time + "\t" + sheep1.transform.position.z + "\t" + sheep2.transform.position.z);
+		print(Time.time + "\t" + sheep1.transform.position.z + "\t" + sheep2.transform.position.z);
+		} else {
+			writer.Close ();
+		}
 	}
 }
---
+++ fisics/unity/Assets/scripts/SimulationManagerData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimulationManagerData : MonoBehaviour {
+
+	public System.Collections.Generic.List<GenomeContainer> tests;
+	public int testNumber;
+}
