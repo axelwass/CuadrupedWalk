bdc7ee81b13c0a9fbe205f5e51f46d7df06332a0
Maximo <maximovs@gmail.com>
2014-02-28 16:23:42 -0300

    paso a carpeta test las pruebas y generación de creatuas de test

---
+++ fisics/unity/Assets/scripts/TestCreature.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class TestCreature : MonoBehaviour {
+	
+	public SimulationsManager simManager;
+	
+	public string creatureFilePath;
+	
+	System.Collections.Generic.List<GenomeContainer> population = new System.Collections.Generic.List<GenomeContainer>();
+	
+	
+	private static TestCreature instance;
+	
+	int generation = 0;
+	
+	public bool generate = true;
+	
+	void Awake(){
+		if(!generate ||instance != null){
+			
+			DestroyImmediate(this.gameObject);
+		}
+		else{
+			instance = this;	
+			DontDestroyOnLoad(this);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+			population.Add(new GenomeContainer(Genome.createFromFile("test/"+creatureFilePath)));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!simManager.isRuningTest()){
+			simManager.runTests(population);
+		}
+	}
+}
--- fisics/unity/Assets/scripts/TestCreatureScript.cs
+++ fisics/unity/Assets/scripts/TestCreatureScript.cs
@@ -9,12 +9,21 @@ public class TestCreatureScript : MonoBehaviour {
 	
 	float elapsedTime = 0;
 	
+	
+	public float testingTime = 20;
+	
 	public string creatureFilePath;
 	
 	private static TestCreatureScript instance;
 	
+	bool firstTime = true;
+	
+	public bool generate = true;
+	
+	Genome genome;
+	
 	void Awake(){
-		if(instance != null){
+		if(!generate || instance != null){
 			DestroyImmediate(this.gameObject);	
 		}
 		else{
@@ -25,7 +34,7 @@ public class TestCreatureScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		Application.LoadLevel(1);
+		genome = Genome.createFromFile(creatureFilePath);
 	}
 	
 	
@@ -34,16 +43,27 @@ public class TestCreatureScript : MonoBehaviour {
 			//Debug.Log("testnumber: " + testNumber);
 			testingCreature = GameObject.Find("testingCreature");//(GameObject)Instantiate(creaturePref);
 			tester = (MoveController)testingCreature.GetComponent("MoveController");
-			tester.testGenome(Genome.createFromFile(creatureFilePath));
+			tester.testGenome(genome);
 			
 			elapsedTime=-0.02f;
 		}
 	}
 	
 	void FixedUpdate () {
-		if(tester!=null){
-			tester.updateState(elapsedTime);
+		if(firstTime){
+			Application.LoadLevel(1);	
+			firstTime = false;
+		} else{
+			if( testingTime!= 0 && elapsedTime >testingTime){
+				float evaluation = tester.getAdvance();//250f - tester.getCuadraticErrorPosition() - tester.getCuadraticErrorRotation();// / (1 + tester.getCuadraticError());
+				evaluation = evaluation<0? 0: evaluation;
+				Debug.Log(" error position: " + tester.getCuadraticErrorPosition() + "-- error rotation: " + tester.getCuadraticErrorRotation() + "-- advance: " + tester.getAdvance() + "-- evaluation: " + evaluation);	
+				testingTime = 0;
+			}
+			if(tester!=null){
+				tester.updateState(elapsedTime);
+			}
+			elapsedTime+=Time.deltaTime;
 		}
-		elapsedTime+=Time.deltaTime;
 	}
 }
