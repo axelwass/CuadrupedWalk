3d9361587c0e749000b9b5aebdb72fffe0b8e96b
axelwass <axelwass@gmail.com>
2014-02-28 20:03:13 -0300

    un buen caso!

--- fisics/unity/Assets/scripts/SimulationManagerData.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SimulationManagerData : MonoBehaviour {
-
-	public System.Collections.Generic.List<GenomeContainer> tests;
-	public int testNumber;
-}
